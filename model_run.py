3c289d3a1bddefb869b9a12b32ea7395edff14c0
alexnicholsamu <alexander.k.nichols@gmail.com>
2023-11-26 18:40:11 +0100

    view

--- model_run.py
+++ model_run.py
@@ -3,9 +3,9 @@ import visualize
 
 # Visualize the results
 if __name__ == "__main__":
-    data_pack = {"coin": "Bitcoin",  # Change for desired coin
+    data_pack = {"coin": "Dogecoin",  # Change for desired coin
                  "learning rate": 0.01,
-                 "number_epochs": 2500,
+                 "number_epochs": 200,
                  "patience": 60}
     actual, predicted, predicted_std, tomorrow_price = train_model.getPredictions(data_pack)  
     print("Tomorrow's predicted price: $" + str(tomorrow_price))
